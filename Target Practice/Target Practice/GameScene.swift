504123630b4015f4744ce2d8796016dbd1acc5c5
Tee Becker <teebecker@Tees-MacBook.local>
2020-11-30 15:34:44 -0800

    added target practice

--- Target Practice/Target Practice/GameScene.swift
+++ Target Practice/Target Practice/GameScene.swift
@@ -8,6 +8,7 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var charNode: SKSpriteNode!
     var snowField: SKEmitterNode!
     var scoreLabel: SKLabelNode!
     var timerLabel: SKLabelNode!
@@ -28,6 +29,7 @@ class GameScene: SKScene {
             bulletsLabel.text = "Bullets Left: \(bulletsLeft)"
         }
     }
+    
     var secondsRemaining = 60
     
     override func didMove(to view: SKView) {
@@ -56,23 +58,41 @@ class GameScene: SKScene {
         addChild(bulletsLabel)
         bulletsLeft = 6
         
-        
         physicsWorld.gravity = .zero
-        createTarget()
+        
+        /// you are already calling this using the targetTimer. no need to do it again
+        //                createTarget()
+        
         
         gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        
         targetTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createTarget), userInfo: nil, repeats: true)
         
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {return}
         let location = touch.location(in: self)
         let tappedNodes = nodes(at: location)
+        
+        for node in tappedNodes{
+            if node.name == "charFriend"{
+                score -= 3
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            }else if node.name == "charEnemy"{
+                score += 5
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }else{
+                score -= 1
+            }
+        }
+        hit()
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
     @objc func countDown(){
         secondsRemaining -= 1
         timerLabel.text = "Timer: \(secondsRemaining)"
@@ -86,60 +106,62 @@ class GameScene: SKScene {
             gameTimer?.invalidate()
         }
     }
+    
+    ///MARK: TODO CHANGE
     @objc func createTarget(){
         
-        let goodTarget = SKSpriteNode(imageNamed: "penguinGood")
-        goodTarget.physicsBody = SKPhysicsBody(texture: goodTarget.texture!, size: goodTarget.size)
-        goodTarget.physicsBody?.linearDamping = 0
-        goodTarget.physicsBody?.angularDamping = 0
-        goodTarget.physicsBody?.categoryBitMask = 1
-        goodTarget.position = CGPoint(x: 0, y: Int.random(in: 60...550))
-
-        let badTarget = SKSpriteNode(imageNamed: "penguinEvil")
-        badTarget.physicsBody = SKPhysicsBody(texture: badTarget.texture!, size: badTarget.size)
-        badTarget.physicsBody?.categoryBitMask = 1
-        badTarget.physicsBody?.linearDamping = 0
-        badTarget.physicsBody?.angularDamping = 0
-        badTarget.position = CGPoint(x: 0, y: Int.random(in: 50...600))
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        /// in line 11, you decleared charnode as a SKSpritenode. There fore you have to initialize this SKSpritenode before you could update its texture.
+        charNode.texture = SKTexture(imageNamed: "penguinGood")
+        charNode.physicsBody = SKPhysicsBody(texture: charNode.texture!, size: charNode.size)
+        charNode.name = "charFriend"
+        charNode.physicsBody?.linearDamping = 0
+        charNode.physicsBody?.angularDamping = 0
+        charNode.physicsBody?.categoryBitMask = 1
+        charNode.position = CGPoint(x: 0, y: Int.random(in: 70...500))
+        /// addchild here. Since on the bottom we are just changing the image texture we dont need to call addchild again.
+        addChild(charNode)
         
+        charNode.texture = SKTexture(imageNamed: "penguinEvil")
+        charNode.position = CGPoint(x: -100, y: Int.random(in: 50...700))
+        charNode.name = "charEnemy"
+        charNode.physicsBody = SKPhysicsBody(texture: charNode.texture!, size: charNode.size)
+        charNode.physicsBody?.linearDamping = 0
+        charNode.physicsBody?.categoryBitMask = 1
+        charNode.physicsBody?.angularDamping = 0
         
-        let num = Int.random(in: 0 ... 3)
-        
-        if num == 0{
-            /// small and fast
-            addChild(goodTarget)
-            goodTarget.xScale = CGFloat(0.7)
-            goodTarget.yScale = goodTarget.xScale
-            goodTarget.physicsBody?.velocity = CGVector(dx: Int.random(in: 500 ... 600), dy: 0)
-            
-        } else if num == 1{
-            /// small and fast
-            addChild(badTarget)
-            badTarget.xScale = CGFloat(0.9)
-            badTarget.yScale = badTarget.xScale
-            badTarget.physicsBody?.velocity = CGVector(dx: Int.random(in: 450 ... 550), dy: 0)
-            
-        } else if num == 2{
-            /// mid and medium speed
-            addChild(goodTarget)
-            goodTarget.xScale = CGFloat(1)
-            goodTarget.yScale = goodTarget.xScale
-            goodTarget.physicsBody?.velocity = CGVector(dx: Int.random(in: 250 ... 450), dy: 0)
-
-        } else if num == 3{
-            /// large and slow
-            addChild(badTarget)
-            badTarget.xScale = CGFloat(1.5)
-            badTarget.yScale = badTarget.xScale
-            badTarget.physicsBody?.velocity = CGVector(dx: 200, dy: 0)
-            
+        if Int.random(in: 0...3) == 0{
+            //small and fast target
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.xScale = CGFloat(0.8)
+            charNode.yScale = charNode.xScale
+            charNode.physicsBody?.velocity = CGVector(dx: Int.random(in: 450...500), dy: 0)
+        }else if Int.random(in: 0...3) == 1{
+            //small and fasr target
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.xScale = CGFloat(0.95)
+            charNode.yScale = charNode.xScale
+            charNode.physicsBody?.velocity = CGVector(dx: Int.random(in: 350...450), dy: 0)
+        }else if Int.random(in: 0...3) == 2{
+            //big and slow target
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.xScale = CGFloat(1.5)
+            charNode.yScale = charNode.xScale
+            charNode.physicsBody?.velocity = CGVector(dx: Int.random(in: 150...200), dy: 0)
+        }else if Int.random(in: 0...3) == 3{
+            //big and slow target
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.xScale = CGFloat(2.0)
+            charNode.yScale = charNode.xScale
+            charNode.physicsBody?.velocity = CGVector(dx: Int.random(in: 100...150), dy: 0)
         }
         
     }
-    
     func hit(){
         isHit = true
         let explosion = SKEmitterNode(fileNamed: "explosion")
-        
+        explosion?.position = charNode.position
+        addChild(explosion!)
+        charNode.removeFromParent()
     }
 }
